e5ce2ef79471bee8d22df2ef45aad77dd9f84689
vinothanS <vinothanvinothan202@gmail.com>
2023-10-05 09:13:16 -0700

    stopwatch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'watch';
+
+  interval:any;
+  time = new Date();
+  startTimer()
+  {
+    this.interval =setInterval(() => {
+      this.time.setSeconds(this.time.getSeconds() +1);
+    })
+  }
+
+  pauseTimer()
+  {
+    clearInterval(this.interval)
+  }
+  resetTimer()
+  {
+    this.time.setSeconds(0);
+  }
 }
